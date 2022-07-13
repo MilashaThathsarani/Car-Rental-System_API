0b78ed2239982dcd4bcff0cf4f6680569a26cf48
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 00:44:41 +0530

    Updated

--- BackEnd/src/main/controller/CustomerController.java
+++ BackEnd/src/main/controller/CustomerController.java
@@ -1,12 +1,30 @@
 package main.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import main.dto.CustomerDTO;
+import main.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/customer")
 @CrossOrigin
 public class CustomerController {
+
+    @Autowired
+    CustomerService service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCustomers() {
+        return new ResponseUtil(200, "Ok", service.getAllCustomers());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCustomer(CustomerDTO dto) {
+        service.saveCustomer(dto);
+        return new ResponseUtil(200, "Saved", null);
+    }
 }
