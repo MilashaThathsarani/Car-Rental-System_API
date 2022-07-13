e81d0ca0ca15123fae5914fc38d1de500949d41e
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 00:45:05 +0530

    Updated

--- BackEnd/src/main/controller/CustomerController.java
+++ BackEnd/src/main/controller/CustomerController.java
@@ -27,4 +27,10 @@ public class CustomerController {
         service.saveCustomer(dto);
         return new ResponseUtil(200, "Saved", null);
     }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO dto) {
+        service.updateCustomer(dto);
+        return new ResponseUtil(200, "Updated", null);
+    }
 }
