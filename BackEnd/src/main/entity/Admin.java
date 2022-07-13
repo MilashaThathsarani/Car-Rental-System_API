425b79d4e907f04bb5b312a028d5d7004c213ecf
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 02:56:55 +0530

    Updated

--- BackEnd/src/main/entity/Admin.java
+++ BackEnd/src/main/entity/Admin.java
@@ -1,4 +1,17 @@
 package main.entity;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Admin {
+    @Id
+    private String adminId;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String username;
+    private String password;
 }
