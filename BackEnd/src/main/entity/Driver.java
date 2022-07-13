ac43d1b318972714b3cf4532ef71463c7066087c
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 02:58:42 +0530

    Updated

--- BackEnd/src/main/entity/Driver.java
+++ BackEnd/src/main/entity/Driver.java
@@ -1,4 +1,23 @@
 package main.entity;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Driver {
+    @Id
+    private String licenceNo;
+    private String name;
+    private String address;
+    private int contactNo;
+    private String nicNo;
+    private String username;
+    private String password;
+    private final boolean availability = true;
+
+    @OneToMany(mappedBy = "driver",cascade = CascadeType.ALL)
+    private List<CarRent> rentals = new ArrayList<>();
 }
+
