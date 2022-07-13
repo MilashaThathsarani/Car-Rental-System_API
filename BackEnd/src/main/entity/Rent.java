ef919f937ba7344ba41bef14440e146f6b9c359d
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 02:59:50 +0530

    Updated

--- BackEnd/src/main/entity/Rent.java
+++ BackEnd/src/main/entity/Rent.java
@@ -1,4 +1,30 @@
 package main.entity;
 
-public class Rent {
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class CarRent {
+    @Id
+    private String rentId;
+    private LocalDate date;
+    private LocalDate pickUpDate;
+    private LocalDate returnDate;
+    private final String status = "Pending";
+
+    @ManyToOne
+    @JoinColumn(name = "customerId", referencedColumnName = "customerId", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "registrationNO", referencedColumnName = "registrationNO", nullable = false)
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "licenceNo", referencedColumnName = "licenceNo", nullable = false)
+    private Driver driver;
 }
+
