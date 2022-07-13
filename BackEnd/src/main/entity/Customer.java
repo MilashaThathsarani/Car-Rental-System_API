c2f316984b4118f99ac7b72e9b131df8aed8598c
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 02:58:12 +0530

    Updated

--- BackEnd/src/main/entity/Customer.java
+++ BackEnd/src/main/entity/Customer.java
@@ -1,4 +1,31 @@
 package main.entity;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String customerId;
+    private String name;
+    private String address;
+    private int contactNo;
+    private String email;
+    private String nicNo;
+    private String nicFrontImg;
+    private String nicBackImg;
+    private String licenceNo;
+    private String licenceImg;
+    private String username;
+    private String password;
+    private final String status = "Pending";
+
+    @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+    private List<CarRent> rentals = new ArrayList<>();
+
+    @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+    private List<Payment> payments = new ArrayList<>();
 }
+
