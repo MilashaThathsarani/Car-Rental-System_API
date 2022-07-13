b4aa3d10125dd49caae6c8415b3e4be8924eea7c
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 02:56:11 +0530

    Updated

--- BackEnd/src/main/dto/DriverDTO.java
+++ BackEnd/src/main/dto/DriverDTO.java
@@ -1,4 +1,17 @@
 package main.dto;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class DriverDTO {
+    private String licenceNo;
+    private String name;
+    private String address;
+    private int contactNo;
+    private String nicNo;
+    private String username;
+    private String password;
+    private final boolean availability = true;
 }
+
