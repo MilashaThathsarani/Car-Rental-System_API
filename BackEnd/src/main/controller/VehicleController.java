67bdcdef3e5af6960089f61c1394a5d460d32d3c
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 02:53:35 +0530

    Updated

--- BackEnd/src/main/controller/VehicleController.java
+++ BackEnd/src/main/controller/VehicleController.java
@@ -1,9 +1,11 @@
 package main.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import main.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/car")
