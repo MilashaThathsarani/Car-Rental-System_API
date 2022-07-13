8185d7cc6416bc85430bbcf3d655ff9d71f61929
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 00:55:20 +0530

    Updated

--- BackEnd/src/main/controller/CustomerController.java
+++ BackEnd/src/main/controller/CustomerController.java
@@ -83,4 +83,33 @@ public class CustomerController {
     public ResponseUtil getAllAcceptedCustomers() {
         return new ResponseUtil(200, "Ok", service.getAllAcceptedCustomers());
     }
+
+    @PutMapping(path = "/up/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil uploadImagesAndPath(@RequestPart("nicf") MultipartFile nicf, @RequestPart("nicb") MultipartFile nicb, @RequestPart("licenceImg") MultipartFile licenceImg, @PathVariable String id) {
+        try {
+            String projectPath = String.valueOf(new File("/media/prageeth/Disk D/ProjectFiles/Easy_Car_Rent"));
+            File uploadsDir = new File(projectPath + "/Customers");
+            uploadsDir.mkdir();
+            nicf.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + nicf.getOriginalFilename()));
+            nicb.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + nicb.getOriginalFilename()));
+            licenceImg.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + licenceImg.getOriginalFilename()));
+
+            String nicfPath = projectPath + "/Customers/" + nicf.getOriginalFilename();
+            String nicbPath = projectPath + "/Customers/" + nicb.getOriginalFilename();
+            String licenceImgPath = projectPath + "/Customers/" + licenceImg.getOriginalFilename();
+
+            service.uploadCustomerImages(nicfPath, nicbPath, licenceImgPath, id);
+
+            return new ResponseUtil(200, "Uploaded", null);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseUtil(500, "Error", null);
+        }
+    }
+
+    @GetMapping(path = "/count",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getCountOfRegisteredCustomers(){
+        return new ResponseUtil(200,"Ok",service.getCountOfCustomersRegistered());
+    }
 }
