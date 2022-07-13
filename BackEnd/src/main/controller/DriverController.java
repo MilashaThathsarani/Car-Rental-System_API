96c9f4972144118ce6cc5c114e5bcb12a7847298
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-14 00:58:27 +0530

    Updated

--- BackEnd/src/main/controller/DriverController.java
+++ BackEnd/src/main/controller/DriverController.java
@@ -1,11 +1,27 @@
 package main.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import main.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/driver")
 @CrossOrigin
 public class DriverController {
+
+    @Autowired
+    DriverService service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllDrivers() {
+        return new ResponseUtil(200, "Ok", service.getAllDrivers());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveDriver(DriverDTO dto) {
+        service.saveDriver(dto);
+        return new ResponseUtil(200, "Saved", null);
+    }
 }
