655b536803eca70b5ddb8528bd87a97a1ace80f0
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-20 19:13:10 +0530

    Updated

--- BackEnd/src/main/java/lk/ijse/spring/controller/MaintenanceController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/MaintenanceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/payment")
+@RequestMapping("api/v1/maintenance")
 @CrossOrigin
-public class PaymentController {
+public class MaintenanceController {
 }
