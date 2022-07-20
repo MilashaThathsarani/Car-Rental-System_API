95ff9c848b86154b375a58e6e7c70a383731fe7b
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-20 15:56:15 +0530

    Updated Vehicle

--- BackEnd/src/main/java/lk/ijse/spring/service/VehicleService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/VehicleService.java
@@ -1,4 +1,16 @@
 package lk.ijse.spring.service;
+import lk.ijse.spring.dto.VehicleDTO;
+
+import java.util.List;
 
 public interface VehicleService {
+    public void saveVehicle(VehicleDTO dto);
+
+    public void deleteVehicle(String id);
+
+    public void updateVehicle(VehicleDTO dto);
+
+    public VehicleDTO searchVehicle(String id);
+
+    public List<VehicleDTO> getAllVehicles();
 }
