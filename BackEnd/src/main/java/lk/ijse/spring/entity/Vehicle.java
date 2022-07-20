a3b8b0ce7c25f9e3ce0b4afecbe629cb5bff0737
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-20 13:53:36 +0530

    Updated Vehicle

--- BackEnd/src/main/java/lk/ijse/spring/entity/Vehicle.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Vehicle.java
@@ -1,35 +1,16 @@
-//package lk.ijse.spring.entity;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@ToString
-//@Entity
-//public class Vehicle {
-//    @Id
-//    private String registrationNO;
-//    private String brand;
-//    private String type;
-//    private int noOfPassengers;
-//    private String transmissionType;
-//    private String fuelType;
-//    private String color;
-//    private String frontViewImg;
-//    private String backViewImg;
-//    private String sideViewImg;
-//    private String internalViewImg;
-//    private double dailyRate;
-//    private double monthlyRate;
-//    private double freeKmForPrice;
-//    private double freeKmForDuration;
-//    private double lossDamageWaiver;
-//    private double priceForExtraKm;
-//    private double completeKm;
-//    private final String status = "Available";
-//
-//    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
-//    private List<CarRent> rentals = new ArrayList<>();
-//
-//    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
-//    private List<Maintenance> maintenances = new ArrayList<>();
-//}
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class Vehicle {
+}
