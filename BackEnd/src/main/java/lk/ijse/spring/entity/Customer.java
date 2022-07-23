549be6c60e7e8a4e76b1fbf5a8f52961b9c1790a
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-23 17:13:54 +0530

    Updated

--- BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,4 +1,28 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String cusId;
+    private String cusEmail;
+    private String cusPassword;
+    private String cusNic;
+    private String cusNicImage;
+    private String cusDriverLicense;
+    private String cusDriverLicenseImage;
+    private String cusAddress;
+    private int cusContactNo;
+    private String cusStatus;
 }
