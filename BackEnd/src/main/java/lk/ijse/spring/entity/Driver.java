0822c2033b31ddc9cfb83b4197fbce36f47caf5a
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-23 17:15:52 +0530

    Updated

--- BackEnd/src/main/java/lk/ijse/spring/entity/Driver.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Driver.java
@@ -1,4 +1,28 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Driver {
+    @Id
+    private String driverId;
+    private String driverName;
+    private String driverEmail;
+    private String driverPassword;
+    private String driverNic;
+    private String driverLicense;
+    private String driverAddress;
+    private int driverContactNo;
+    private int driverAge;
+    private String driverGender;
 }
