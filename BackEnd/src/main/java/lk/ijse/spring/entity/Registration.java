ea2703b85edae30ec6d57bf5db3742a1d82d56ce
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-23 17:19:07 +0530

    Updated

--- BackEnd/src/main/java/lk/ijse/spring/entity/Registration.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Registration.java
@@ -1,4 +1,27 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Registration {
+    @Id
+    private String email;
+    private String password;
+    private String nic;
+    private String nicImage;
+    private String dlicense;
+    private String dlicenseImage;
+    private String address;
+    private int contactNo;
+    private String cusStatus;
 }
