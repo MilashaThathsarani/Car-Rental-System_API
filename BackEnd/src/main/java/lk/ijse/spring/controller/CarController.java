aa8bbd561dd15cd794793f20335a7b6876dd30f1
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-24 12:06:22 +0530

    Updated

--- BackEnd/src/main/java/lk/ijse/spring/controller/CarController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -1,4 +1,65 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.AdminAccountDTO;
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class CarController {
+    @Autowired
+    private CarService carService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCars() {
+        return new ResponseUtil(200,"Ok",carService.getAllCars());
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCar(@ModelAttribute CarDTO carDTO) {
+        carService.saveCar(carDTO);
+        return new ResponseUtil(200,"Ok",null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCar(@RequestBody CarDTO carDTO) {
+        carService.updateCar(carDTO);
+        return new ResponseUtil(200,"Ok",null);
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCar(@RequestParam String id) {
+        carService.deleteCar(id);
+        return new ResponseUtil(200,"Ok",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCar(@PathVariable String id) {
+        return new ResponseUtil(200,"Ok",carService.searchCar(id));
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public boolean saveFile(@RequestPart("myFile") MultipartFile myFile) {
+        System.out.println(myFile.getOriginalFilename());;
+        try {
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+            File uploadsDir = new File(projectPath + "/uploads");
+            uploadsDir.mkdir();
+            myFile.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + myFile.getOriginalFilename()));
+            return true;
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
