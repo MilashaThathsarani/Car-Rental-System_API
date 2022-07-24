5db10af8bab0dfc4df5c807f347ffd80f1f3b831
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-24 10:20:10 +0530

    Updated

--- BackEnd/src/main/java/lk/ijse/spring/service/Impl/BookingServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/Impl/BookingServiceImpl.java
@@ -1,13 +1,18 @@
 package lk.ijse.spring.service.Impl;
 
+import lk.ijse.spring.dto.AdminAccountDTO;
 import lk.ijse.spring.dto.BookingDTO;
+import lk.ijse.spring.entity.AdminAccount;
+import lk.ijse.spring.entity.Booking;
 import lk.ijse.spring.repo.BookingRepo;
 import lk.ijse.spring.service.BookingService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class BookingServiceImpl implements BookingService {
 
@@ -18,27 +23,45 @@ public class BookingServiceImpl implements BookingService {
     ModelMapper modelMapper;
     @Override
     public void addBooking(BookingDTO bookingDTO) {
-
+        if (!bookingRepo.existsById(bookingDTO.getRequestNumber())) {
+            bookingRepo.save(modelMapper.map(bookingDTO, Booking.class));
+        } else {
+            throw new RuntimeException("Customer Already Exist..!");
+        }
     }
 
     @Override
     public void deleteBooking(String s) {
-
+        if (bookingRepo.existsById(s)){
+            bookingRepo.deleteById(s);
+        }else{
+            throw new RuntimeException("Please check the Customer ID.. No Such Customer..!");
+        }
     }
 
     @Override
     public BookingDTO searchBooking(String s) {
-        return null;
+        if (bookingRepo.existsById(s)){
+            return modelMapper.map(bookingRepo.findById(s).get(), BookingDTO.class);
+        }else{
+            throw new RuntimeException("No Customer For "+s+" ..!");
+        }
     }
 
     @Override
     public ArrayList<BookingDTO> getAllBookings() {
-        return null;
+        List<Booking> all = bookingRepo.findAll();
+        return modelMapper.map(all,new TypeToken<List<BookingDTO>>(){
+        }.getType());
     }
 
     @Override
     public void updateBooking(BookingDTO bookingDTO) {
-
+        if (bookingRepo.existsById(bookingDTO.getRequestNumber())) {
+            bookingRepo.save(modelMapper.map(bookingDTO,Booking.class));
+        } else {
+            throw new RuntimeException("No Such Customer To Update..! Please Check the ID..!");
+        }
     }
 
     @Override
