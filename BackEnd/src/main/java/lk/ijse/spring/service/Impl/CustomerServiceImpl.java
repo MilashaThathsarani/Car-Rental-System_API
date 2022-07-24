ff8c16f6ce4a729e55e77a04c7ebd15e9caf4404
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-24 10:35:42 +0530

    Updated

--- BackEnd/src/main/java/lk/ijse/spring/service/Impl/CustomerServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/Impl/CustomerServiceImpl.java
@@ -1,9 +1,10 @@
 package lk.ijse.spring.service.Impl;
-
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -17,26 +18,44 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public void saveCustomer(CustomerDTO dto) {
-
+        if (!customerRepo.existsById(dto.getCusId())) {
+            customerRepo.save(modelMapper.map(dto, Customer.class));
+        } else {
+            throw new RuntimeException("Customer Already Exist..!");
+        }
     }
 
     @Override
     public void deleteCustomer(String id) {
-
+        if (customerRepo.existsById(id)){
+            customerRepo.deleteById(id);
+        }else{
+            throw new RuntimeException("Please check the Customer ID.. No Such Customer..!");
+        }
     }
 
     @Override
     public void updateCustomer(CustomerDTO dto) {
-
+        if (customerRepo.existsById(dto.getCusId())) {
+            customerRepo.save(modelMapper.map(dto,Customer.class));
+        } else {
+            throw new RuntimeException("No Such Customer To Update..! Please Check the ID..!");
+        }
     }
 
     @Override
     public CustomerDTO searchCustomer(String id) {
-        return null;
+        if (customerRepo.existsById(id)){
+            return modelMapper.map(customerRepo.findById(id).get(), CustomerDTO.class);
+        }else{
+            throw new RuntimeException("No Customer For "+id+" ..!");
+        }
     }
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+        List<Customer> all = customerRepo.findAll();
+        return modelMapper.map(all,new TypeToken<List<CustomerDTO>>(){
+        }.getType());
     }
 }
